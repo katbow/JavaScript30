aec9588256ef9a10c315669311c9738d1452d515
katbow <katherine.bowler@gmail.com>
2016-12-14 17:00:35 +0000

    Add CSS variables and change with JS

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,14 @@
+var inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate () {
+  var suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(function (input) {
+  input.addEventListener('change', handleUpdate)
+})
+
+inputs.forEach(function (input) {
+  input.addEventListener('mousemove', handleUpdate)
+})
