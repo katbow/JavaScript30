4746c76f67aed01700a93a78d292f619d9239169
katbow <katherine.bowler@gmail.com>
2016-12-14 16:05:19 +0000

    Add and remove transitions for sec, hours, mins
    Separates add & remove transition into own functions

--- 02 - JS + CSS Clock - Complete/main.js
+++ 02 - JS + CSS Clock - Complete/main.js
@@ -4,23 +4,32 @@ var hourHand = document.querySelector('.hour-hand')
 
 function setDate () {
   var now = new Date()
+
   var seconds = now.getSeconds()
   var secondsDegrees = ((seconds / 60) * 360) + 90
-  // could apply below to secs, mins, hours to avoid '90deg glitch'
-  if (secondsDegrees === 90) {
-    secondHand.className = 'hand second-hand no-transition'
-  } else if (secondsDegrees === 96) {
-    secondHand.className = 'hand second-hand'
-  }
+  if (secondsDegrees === 90) removeTransition(secondHand)
+  else if (secondsDegrees === 96) addTransition(secondHand)
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
   var mins = now.getMinutes()
   var minsDegrees = ((mins / 60) * 360) + 90
+  if (minsDegrees === 90) removeTransition(minHand)
+  else if (minsDegrees === 96) addTransition(minHand)
   minHand.style.transform = `rotate(${minsDegrees}deg)`
 
   var hours = now.getHours()
   var hoursDegrees = ((hours / 12) * 360) + 90
+  if (hoursDegrees === 90) removeTransition(hourHand)
+  else if (hoursDegrees === 120) addTransition(hourHand)
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+}
+
+function removeTransition (el) {
+  el.classList.add('no-transition')
+}
+
+function addTransition (el) {
+  el.classList.remove('no-transition')
 }
 
 setInterval(setDate, 1000)
