3b71ac492660663fef33c0995a89a42c303b6f35
katbow <katherine.bowler@gmail.com>
2016-12-15 18:08:38 +0000

    Change to toggle function instead of add/remove functions

--- 02 - JS + CSS Clock - Complete/main.js
+++ 02 - JS + CSS Clock - Complete/main.js
@@ -7,29 +7,22 @@ function setDate () {
 
   var seconds = now.getSeconds()
   var secondsDegrees = ((seconds / 60) * 360) + 90
-  if (secondsDegrees === 90) removeTransition(secondHand)
-  else if (secondsDegrees === 96) addTransition(secondHand)
+  if (secondsDegrees === 90 || secondsDegrees === 96) toggleTransition(secondHand)
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
   var mins = now.getMinutes()
   var minsDegrees = ((mins / 60) * 360) + 90
-  if (minsDegrees === 90) removeTransition(minHand)
-  else if (minsDegrees === 96) addTransition(minHand)
+  if (minsDegrees === 90 || minsDegrees === 96) toggleTransition(minHand)
   minHand.style.transform = `rotate(${minsDegrees}deg)`
 
   var hours = now.getHours()
   var hoursDegrees = ((hours / 12) * 360) + 90
-  if (hoursDegrees === 90) removeTransition(hourHand)
-  else if (hoursDegrees === 120) addTransition(hourHand)
+  if (hoursDegrees === 90 || hoursDegrees === 120) toggleTransition(hourHand)
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
-function removeTransition (el) {
-  el.classList.add('no-transition')
-}
-
-function addTransition (el) {
-  el.classList.remove('no-transition')
+function toggleTransition (el) {
+  el.classList.toggle('no-transition')
 }
 
 setInterval(setDate, 1000)
